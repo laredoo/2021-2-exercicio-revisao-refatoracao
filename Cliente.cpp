fe8b17f4ef39d956d7a85cb29cede7ea9b46b14e
laredoo <86807591+laredoo@users.noreply.github.com>
2022-01-26 17:39:07 -0300

    Update Client.cpp
    -Mudar a formatação de 'print()' para ficar menos confuso
    	-Alterar o método 'print()' para adaptar aos atributos privados.
    	-Acrescentar os novos métodos 'get'

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,23 @@
 #include <string>
 #include "Cliente.hpp"
 
+std::string Cliente::getNome(){
+	return this->Nome;
+}
+
+std::string Cliente::getEndereco(){
+	return this->Enderecco;
+}
+
+std::string Cliente::getCep(){
+	return this->Cep;
+}
+
+std::string Cliente::getAlturaDosPais(){
+	return this->AlturaDosPais;
+}
+
 void Cliente::print(){
-
-	std::cout << "[Cliente]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  Endereco: " << endereco << endl
-		 << "  CEP: " << Cep << endl;
-
+	std::cout << "[Cliente]" << endl << "  Nome: " << this->getNome() << endl << "  Endereco: " << this->getEndereco << endl << "  CEP: " << this->getCep() << endl;
 }
 
