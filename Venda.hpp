0c536cb915eafc36ba0bb51439b562d12b102cac
laredoo <86807591+laredoo@users.noreply.github.com>
2022-01-26 18:23:54 -0300

    4805550e9b40a01e5ba1f59358fbe171
    -não é uma boa prática incluir um namespace
    	-apagar comentarios
    	-implementação de métodos devem ser separados de seus contratos
    	-atributos devem ser declarados como private.

--- Venda.hpp
+++ Venda.hpp
@@ -6,37 +6,16 @@
 #include "Cliente.hpp"
 #include "Especialista.hpp"
 
-using namespace std;
 
 class Venda{
 
     public:
-    double VALOR;
-    string descricao;
-    Especialista esp;
-    string cliente;
-
-
-    void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-
-        //std::cout << " : ";
-        //cout << cliente;
-    }
-
-    /*void print() {
-
-        std::cout << "Especialista: ";
-        cout << esp.nome;
-
-        std::cout << " Cliente: ";
-        cout << cliente;
-    }*/
+        void print();
+    private:
+        double VALOR;
+        std::string descricao;
+        std::Especialista esp;
+        std::string cliente;
 };
 
 #endif
